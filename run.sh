87865d29d2afbe0afcaa818850390c3d800fd5e6
lulardou <lulostupnik2003@gmail.com>
2024-09-23 12:12:38 -0300

    run

--- run.sh
+++ run.sh
@@ -6,6 +6,7 @@ sed -r "s/\x1B\[[0-9;]*[a-zA-Z]//g" make_out.txt > compile_out.txt #fixes with n
 rm make_out.txt
 sudo chmod 777 Image/x64BareBonesImage.qcow2
 qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512
+#for audio use the command below:
 #qemu-system-x86_64 -hda Image/x64BareBonesImage.qcow2 -m 512 -audiodev pa,id=speaker -machine pcspk-audiodev=speaker
 sudo docker exec -it SO make clean -C/root/Toolchain
 sudo docker exec -it SO make clean -C/root/
