14faaba14d479b3ae9f26bd7f240287c82703e76
mrossisei <mrossiseifert@itba.edu.ar>
2024-10-03 17:03:52 -0300

    load_stack()

--- Kernel/include/libs/stack.h
+++ Kernel/include/libs/stack.h
@@ -1,23 +1,27 @@
 
+
+// todo -> ver si es así o al revez o no se
+
+
 #ifndef STACK_H
 #define STACK_H
 
 #include <stdint.h>
 
+#define GPR_QTY 15 // son los registros que usamos en pushState y popState
 
-#define GPR_QTY 10 // todo -> cambiar
-
-// todo -> ver si es así o al revez o no se
-typedef struct stack{
-    uint64_t rip;
-    uint64_t cs;
-    uint64_t rflags;
-    uint64_t rsp;
-    uint64_t ss;
+typedef struct stack
+{
+    uint64_t gpr[GPR_QTY];  // inicializado en 0 o basura
+    uint64_t rip;           // el indicado por el creador del proceso
+    uint64_t cs;            // 0x8
+    uint64_t rflags;        // 0x202
+    uint64_t rsp;           // TODO -> ¿hay que hacer una cuenta acá?
+    uint64_t ss;            // 0x0
     // align ni idea
-    uint64_t gpr[GPR_QTY]; // inicializado en 0
-}stack;
 
-void load_stack(uint64_t tip, uint64_t rsp);
+} stack;
+
+uint64_t load_stack(uint64_t rip, uint64_t rsp);
 
 #endif
--- Kernel/libs/stack.c
+++ Kernel/libs/stack.c
@@ -2,7 +2,17 @@
 
 
 
-void load_stack(uint64_t rip, uint64_t rsp){
-    return;
-}
+uint64_t load_stack(uint64_t rip, uint64_t rsp)
+{
+    stack * my_stack = (stack *) (rsp - sizeof(stack));
+    for(int i = 0; i < GPR_QTY; i++){
+        my_stack->gpr[i] = 0;
+    }
+    my_stack->rip = rip;
+    my_stack->cs = 0x8;
+    my_stack->rflags = 0x202;
+    my_stack->rsp = rsp;
+    my_stack->ss = 0x0;
 
+    return (uint64_t) my_stack;
+}
