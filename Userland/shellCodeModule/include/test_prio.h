bb240031d341524e9be59e4388a7f8e588d52e6b
lulardou <lulostupnik2003@gmail.com>
2024-10-14 09:53:22 -0300

    fixes

--- Userland/shellCodeModule/include/test_prio.h
+++ Userland/shellCodeModule/include/test_prio.h
@@ -6,10 +6,11 @@
 #include <test_util.h>  
 
 #define MINOR_WAIT 1000000 // TODO: Change this value to prevent a process from flooding the screen
-// #define WAIT 1000000000      // TODO: Change this value to make the wait long enough to see theese processes beeing run at least twice
-// #define WAIT_STRING "30000000"
-#define WAIT 100000000      // TODO: Change this value to make the wait long enough to see theese processes beeing run at least twice
-#define WAIT_STRING "100000000"
+
+#define WAIT 1000000000      // TODO: Change this value to make the wait long enough to see theese processes beeing run at least twice
+#define WAIT_STRING "30000000"
+// #define WAIT 100000000      // TODO: Change this value to make the wait long enough to see theese processes beeing run at least twice
+// #define WAIT_STRING "100000000"
 
 
 #define TOTAL_PROCESSES 3
