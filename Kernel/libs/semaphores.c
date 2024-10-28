8438e40c8d218651e9669b4041670543dfa5d1e5
mrossisei <mrossiseifert@itba.edu.ar>
2024-10-28 13:19:28 -0300

    sem_close and sem_open

--- Kernel/libs/semaphores.c
+++ Kernel/libs/semaphores.c
@@ -5,10 +5,8 @@
 typedef struct sem_structure{
     uint64_t value;
     uint8_t lock; 
-    uint8_t not_free;
+    uint8_t not_free; // cantidad de procesos que tienen abierto al semáforo
     queue_adt queue;
-    ordered_list_adt process_list;
-    
 } sem_structure;
 
 sem_structure sem_array[SEM_AMOUNT] = {0};
@@ -27,10 +25,11 @@ int64_t my_sem_open(int64_t sem_id, int value){
 
     acquire(&sem_array[sem_id].lock);
     if(sem_array[sem_id].not_free){ 
-        int to_return = add_ordered_list(sem_array[sem_id].process_list, get_running());
+        sem_array[sem_id].not_free++;
         release(&sem_array[sem_id].lock);
-        return to_return;
+        return 1; // retornar otra cosa para ver que esté funcionando
     }
+
     // Si es el primero en abrirlo crea el semáforo
 
     queue_adt queue = new_queue();
@@ -38,46 +37,32 @@ int64_t my_sem_open(int64_t sem_id, int value){
         release(&sem_array[sem_id].lock);
         return -1;
     }
-    ordered_list_adt list = new_ordered_list(cmp); // TODO -> CMP!!!!
-    if(list == NULL){
-        free_queue(queue);
-        release(&sem_array[sem_id].lock);
-        return -1;
-    }
-    if(add_ordered_list(list, get_running()) == -1){
-        free_queue(queue);
-        free_ordered_list(list);
-        release(&sem_array[sem_id].lock);
-        return -1;
-    }
-
 
     sem_array[sem_id].not_free = 1;
-    sem_array[sem_id].lock = 1; // todo -> invertir para acquire / release
+    sem_array[sem_id].lock = 1; 
     sem_array[sem_id].value = value;
     sem_array[sem_id].queue = queue;
-    sem_array[sem_id].process_list = list;
     
     release(&sem_array[sem_id].lock);
-    return 1; // todo -> ??
+    return 1; 
 }
 
 
 int64_t my_sem_wait(int64_t sem_id){
+
     if((sem_id >= SEM_AMOUNT) || (sem_id < 0) ){
         return -1;
     }
 
-    
-    // todo -> chequear que el semaforo esté abierto para el proceso que quiere hacer wait
-    // todo -> ¿hacer acquire antes? ACQUIRE !!!
+    // CREO QUE DESDE ACÁ
     acquire(&sem_array[sem_id].lock);
-    if(!sem_array[sem_id].not_free /*|| (belongs_ordered_list(sem_array[sem_id].process_list, get_running()))*/){ // todo -> ¿está bien?
+    if(!sem_array[sem_id].not_free ){ 
         release(&sem_array[sem_id].lock);
         return -1;
     }
 
-    release(&sem_array[sem_id].lock);
+    release(&sem_array[sem_id].lock); // todo -> creo que es innecesario
+    // HASTA ACÁ SE PUEDE BORRAR
 
     while(1){
 
@@ -105,31 +90,38 @@ int64_t my_sem_wait(int64_t sem_id){
 
 int64_t my_sem_post(int64_t sem_id) {
 
+    if((sem_id >= SEM_AMOUNT) || (sem_id < 0) ){
+        return -1;
+    }
+
     acquire(&sem_array[sem_id].lock);
-    
-    // if(!belongs_ordered_list(sem_array[sem_id].process_list, get_pcb(get_pid()))){
-    //     return -1;
-    // }
+
+    if(!sem_array[sem_id].not_free){
+        release(&sem_array[sem_id].lock);
+        return -1;
+    }
     
     sem_array[sem_id].value++;
     PCB * to_unblock ;
+
     if(!queue_is_empty(sem_array[sem_id].queue)){
         to_unblock = dequeue(sem_array[sem_id].queue);           
-        if(to_unblock->status != BLOCKED){
-            sem_id++;
-            sem_id--;
-        }
         ready(to_unblock);
     }
 
     release(&sem_array[sem_id].lock);
-    PCB * p = get_pcb(get_pid());
+    // PCB * p = get_pcb(get_pid());
     scheduler_yield();
     return 1;
 }
 
 // todo -> chequear está función !!!!
 int64_t my_sem_close(int64_t sem_id){
+
+    if((sem_id >= SEM_AMOUNT) || (sem_id < 0) ){
+        return -1;
+    }
+
     PCB * pcb;
 
     acquire(&sem_array[sem_id].lock);
@@ -138,17 +130,26 @@ int64_t my_sem_close(int64_t sem_id){
         release(&sem_array[sem_id].lock);
         return -1;
     }
-    
-    if(delete_ordered_list(sem_array[sem_id].process_list, get_running()) == -1){
-        release(&sem_array[sem_id].lock);
-        return -1;
-    }
-    
-    if(is_empty_ordered_list(sem_array[sem_id].process_list)){
+
+    sem_array[sem_id].not_free --;
+    if(sem_array[sem_id].not_free == 0){
+        while(!queue_is_empty(sem_array[sem_id].queue)){
+            pcb = dequeue(sem_array[sem_id].queue);
+            ready(pcb);
+        }
         free_queue(sem_array[sem_id].queue);
-        free_ordered_list(sem_array[sem_id].process_list);
-        sem_array[sem_id].not_free = 0;
     }
+    
+    // if(delete_ordered_list(sem_array[sem_id].process_list, get_running()) == -1){
+    //     release(&sem_array[sem_id].lock);
+    //     return -1;
+    // }
+    
+    // if(is_empty_ordered_list(sem_array[sem_id].process_list)){
+    //     free_queue(sem_array[sem_id].queue);
+    //     free_ordered_list(sem_array[sem_id].process_list);
+    //     sem_array[sem_id].not_free = 0;
+    // }
     
     release(&sem_array[sem_id].lock);
     return 1;
